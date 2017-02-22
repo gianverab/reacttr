dc2829fa0d1d30e069b1a6490ca7d15ae7f577f8
Giancarlo Vera Burgos <Giancarlo Vera Burgos>
2017-02-22 01:38:10 -0300

    Binding object this in methods

--- src/components/InputText/index.jsx
+++ src/components/InputText/index.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import style from './input-text.css'
 
 class InputText extends Component {
   render () {
     return (
-      <div>InputText</div>
+      <div className={style.root}>
+        <form className={style.form} onSubmit={this.props.onSendText}>
+          <textarea name="text" className={style.text}></textarea>
+          <div className={style.buttons}>
+            <button className={style.close} onClick={this.props.onCloseText}>Close</button>
+            <button className={style.send} type="submit">Send</button>
+          </div>
+        </form>
+      </div>
     )
   }
 }
