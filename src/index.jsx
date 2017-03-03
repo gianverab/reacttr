e70252879ebc54677d7257a6b9ae1ced769e9346
Giancarlo Vera Burgos <Giancarlo Vera Burgos>
2017-03-02 21:53:17 -0300

    Adding firebase to our app

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
+import firebase from 'firebase'
 
 import App from './components/App'
+
+// Initialize Firebase
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDjEmuTDp5yA02oUVwsV6pyYhdiWO90ZrE',
+  authDomain: 'reacttr-65405.firebaseapp.com',
+  databaseURL: 'https://reacttr-65405.firebaseio.com',
+  storageBucket: 'reacttr-65405.appspot.com',
+  messagingSenderId: '627392721393'
+})
 
 render(<App />, document.getElementById('root'))
